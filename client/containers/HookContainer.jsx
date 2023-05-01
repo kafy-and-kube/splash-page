f981a618c4752c2dda7adf1565c7d0f83cdff498
Joe Ostrow <josephostrow@joes-air.mynetworksettings.com>
2023-05-01 15:08:54 -0400

    adds FAQs, collapsible when clicking on question

--- client/containers/HookContainer.jsx
+++ client/containers/HookContainer.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 export default function HookContainer() {
   return (
-    <div id='hook'>
+    <div id="hook">
       <h1>
         KoffeeTable brings your Apache Kafka visualization needs to the table
       </h1>
       <h2>Integrate and visualize your Apache Kafka clusters</h2>
-      <div class='buttons-container'>
-        <button id='koffeetable-btn'>Get KoffeeTable</button>
-        <button id='learnmore-btn'>
-          Learn more
-        </button>
+      <div className="buttons-container">
+        <button id="koffeetable-btn">Get KoffeeTable</button>
+        <button id="learnmore-btn">Learn more</button>
       </div>
     </div>
   );
