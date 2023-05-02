558b32ab28ae802c3b92f664e26a51a9ed9482ef
Mattholee <matthew0505@gmail.com>
2023-05-02 01:54:20 -0400

    will add media queries for movile

--- client/components/AboutUs.jsx
+++ client/components/AboutUs.jsx
@@ -5,7 +5,6 @@ export default function AboutUs() {
   const names = ['Gavin', 'Jonas', 'Joe', 'Jonathan', 'Matthew']
   const photos= []
 
-
   const profiles = []
   for (let i = 0; i < names.length; i++) {
     profiles.push(<Profile photo={photos[i]} name={names[i]}/>)
